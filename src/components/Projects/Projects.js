69668e168c7a7db3a8dd2e8b6d66e0e42ef4f905
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2024-04-08 18:22:55 +0800

    added projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,52 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+
+// Images Imports
 import ateneoTrade from "../../Assets/Portfolio/ateneo-trade.png";
+import brigada from "../../Assets/Portfolio/brigada.png";
+import byte from "../../Assets/Portfolio/byte.png";
+import managerio from "../../Assets/Portfolio/managerio.png";
 
 function Projects() {
+  const webProjectsData = [
+    {
+      imgPath: ateneoTrade,
+      isBlog: false,
+      title: "Ateneo Trade",
+      description:
+        "Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages.",
+      demoLink: "https://ateneo-trade.vercel.app/",
+    },
+    {
+      imgPath: managerio,
+      isBlog: false,
+      title: "Manager.io",
+      description: "An HR Management application",
+      demoLink: "https://managerio.vercel.app/employees",
+    },
+    {
+      imgPath: byte,
+      isBlog: false,
+      title: "BYTE",
+      description: "A website for BYTE",
+      demoLink: "#",
+    },
+    {
+      imgPath: brigada,
+      isBlog: false,
+      title: "Brigada E-skwela",
+      description: "An advocacy website for brigada e-skwela",
+      demoLink: "https://www.brigadaeskwela.org/",
+    },
+    {
+      imgPath: brigada,
+      isBlog: false,
+      title: "Brigada E-skwela",
+      description: "An advocacy website for brigada e-skwela",
+      demoLink: "https://www.brigadaeskwela.org/",
+    },
+  ];
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -15,17 +58,32 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are the projects that I have built throughout the years.
         </p>
+
+        <h3 className="project-heading">
+          Web <strong className="teal">Development</strong>
+        </h3>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={ateneoTrade}
-              isBlog={false}
-              title="Ateneo Trade"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              demoLink="https://ateneo-trade.vercel.app/"
-            />
-          </Col>
+          {webProjectsData.map((project, index) => (
+            <Col
+              xl={3}
+              lg={4}
+              md={6}
+              sm={12}
+              className="project-card"
+              key={index}
+            >
+              <ProjectCard {...project} />
+            </Col>
+          ))}
         </Row>
+        <h3 className="project-heading">
+          Mobile <strong className="teal">Development</strong>
+          <p>Coming soon</p>
+        </h3>
+        <h3 className="project-heading">
+          Data <strong className="teal">Science</strong>
+          <p>Coming soon</p>
+        </h3>
       </Container>
     </Container>
   );
